ef5313b7503cca09a23bb25fe4bc82a514fbee76
Xiaoping Zhang <zhan0566@algonquinlive.com>
2022-06-06 12:24:55 -0400

    lab3-(1-5 questions)

--- app/src/main/java/com/cst2335/androidlabs2/MainActivity.java
+++ app/src/main/java/com/cst2335/androidlabs2/MainActivity.java
@@ -2,24 +2,49 @@ package com.cst2335.androidlabs2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+      public final static String TAG ="MainActivity";
+      public final static String PREFERENCES_FILE = "MyData";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_constraint);
-        Button btn = findViewById(R.id.button2);
+        //Coding from here
+        setContentView(R.layout.activity_main_linear);
+
+        SharedPreferences prefs = getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        String previous = prefs.getString("ReserveName", "Default Value");
+        TextView reverseMessage = findViewById(R.id.reserved_name);
+        reverseMessage.setText(previous);
+
+        EditText editEmail = findViewById(R.id.editEmail);
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(clk -> {
+            SharedPreferences.Editor writer = prefs.edit();
+            writer.putString("ReserveName", editEmail.getText().toString());
+            writer.apply(); //save to disk
+            finish();
+        });
+
+
+
+       /** Button btn = findViewById(R.id.button2);
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -45,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
                 sb.setAction(getResources().getString(R.string.undoMessage), click -> cb.setChecked(!isChecked));
                 sb.show();
             }
-        });
+        });*/
+
+
     }
+
 }
