98a5f46b532cc6d9bbcdc58cb96c0b15af544647
Xiaoping Zhang <zhan0566@algonquinlive.com>
2022-06-06 15:49:48 -0400

    lab3-(from question 6)

---
+++ app/src/main/java/com/cst2335/androidlabs2/ProfileActivity.java
@@ -0,0 +1,20 @@
+package com.cst2335.androidlabs2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+
+
+
+    }
+}
