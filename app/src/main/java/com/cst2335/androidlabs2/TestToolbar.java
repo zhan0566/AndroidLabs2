47f4fff4d1525bf4a8e3177fd8e63f51f73e518d
Xiaoping Zhang <zhan0566@algonquinlive.com>
2022-07-21 22:12:05 -0400

    Lab7

---
+++ app/src/main/java/com/cst2335/androidlabs2/TestToolbar.java
@@ -0,0 +1,14 @@
+package com.cst2335.androidlabs2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestToolbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_toolbar);
+    }
+}
